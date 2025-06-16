a1be999d1897c46b485400fdc62e7f58be0087bf
rajumanoj333 <manojkasula.mj@gmail.com>
2025-06-16 09:43:48 +0000

    Add test file with intentional issues

---
+++ test-file.py
@@ -0,0 +1,13 @@
+# TODO: This is a test file
+def hello_world():
+    print("Hello, World!")  # Debug statement
+    return "Hello"
+
+# This is a very long function that might trigger the large file warning
+def long_function():
+    line1 = "This is line 1"
+    line2 = "This is line 2"
+    line3 = "This is line 3"
+    # ... (add more lines if needed)
+    console.log("Debug message")  # This will trigger a warning
+    return "long function"
